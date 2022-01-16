12bb3f386e3881f8ade1f1858e19f1997352ac7d
MarioLuiz <marioluiz.arruda@gmail.com>
2022-01-15 23:20:36 -0400

    Testes com Try Catch

--- java-pilha/src/main/Fluxo.java
+++ java-pilha/src/main/Fluxo.java
@@ -2,23 +2,27 @@ package main;
 
 public class Fluxo {
 
-    public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		try {
+			metodo1();
+		} catch (Exception e) {
+			System.out.println("Deu Ruim : " + e);
+		}
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
 
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Fim do metodo2");
-    }
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+		}
+	}
 }
