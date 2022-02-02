c33b189a0eeb7a8886dbfb700cb88655907dbf75
MarioLuiz <marioluiz.arruda@gmail.com>
2022-02-01 20:40:34 -0400

    Mais testes com String e StringBuilder

--- bank-herdado/src/br/com/bytebank/banco/teste/TesteString.java
+++ bank-herdado/src/br/com/bytebank/banco/teste/TesteString.java
@@ -13,6 +13,42 @@ public class TesteString {
 		System.out.println(nome);
 		System.out.println(outra);
 		System.out.println(outraLower);
+		
+		char c = nome.charAt(3); //char i
+		System.out.println(c);
+
+		int pos = nome.indexOf("rio");
+		System.out.println(pos);
+
+		String sub = nome.substring(1);
+		System.out.println(sub);
+		
+		for(int i = 0; i < nome.length(); i++) {
+		    System.out.println(nome.charAt(i));
+		} 
+		
+		String texto = "Socorram";
+		texto = texto.concat("-");
+		texto = texto.concat("me");
+		texto = texto.concat(", ");
+		texto = texto.concat("subi ");
+		texto = texto.concat("no ");
+		texto = texto.concat("ônibus ");
+		texto = texto.concat("em ");
+		texto = texto.concat("Marrocos");
+		System.out.println(texto); 
+		
+		StringBuilder builder = new StringBuilder("Socorram");
+		builder.append("-");
+		builder.append("me");
+		builder.append(", ");
+		builder.append("subi ");
+		builder.append("no ");
+		builder.append("ônibus ");
+		builder.append("em ");
+		builder.append("Marrocos");
+		String texto2 = builder.toString();
+		System.out.println(texto2);
 	}
 
 }
