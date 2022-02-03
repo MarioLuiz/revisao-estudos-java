90f7ac3e6af273458b441bff42280d2cacce6593
MarioLuiz <marioluiz.arruda@gmail.com>
2022-02-02 20:51:42 -0400

    Praticando com a classe Object

--- bank-herdado/src/br/com/bytebank/banco/model/Cliente.java
+++ bank-herdado/src/br/com/bytebank/banco/model/Cliente.java
@@ -5,8 +5,14 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String profissao;
-
-    public String getNome() {
+    
+    public Cliente(String nome, String cpf, String profissao) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.profissao = profissao;
+	}
+    
+	public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
