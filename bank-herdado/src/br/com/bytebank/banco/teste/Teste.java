7334f1122f440765ff1448f8687c07f9cf40f02b
MarioLuiz <marioluiz.arruda@gmail.com>
2022-02-04 22:48:55 -0400

    Teste com Arrays Java

--- bank-herdado/src/br/com/bytebank/banco/teste/Teste.java
+++ bank-herdado/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,27 +1,33 @@
 package br.com.bytebank.banco.teste;
 
-import br.com.bytebank.banco.model.Cliente;
-import br.com.bytebank.banco.model.ContaCorrente;
-import br.com.bytebank.banco.model.ContaPoupanca;
-
 public class Teste {
+	
+	//Array []
 	public static void main(String[] args) {
 		
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(true);
+		int idades[] = new int[5];
 		
-		Object cc = new ContaCorrente(22, 2020);
-		Object cp = new ContaPoupanca(22, 32);
-		Object cliente = new Cliente("Mario Luiz", "027.789.789-85", "Desenvolvedor");
+		int idade1 = 29;
 		
-		System.out.println(cc.toString());
-		System.out.println(cp.toString());
+		idades[0] = idade1;
 		
-		println(cliente);
-	}
-	
-	static void println(Object referencia) {
-	System.out.println(referencia.toString());
+		int idade = idades[0];
+		
+		
+		int idade2 = 30;
+		int idade3 = 31;
+		int idade4 = 32;
+		int idade5 = 33;
+		
+		System.out.println("Idade: " + idade);
+		System.out.println(idades.length);
+		
+		for (int i = 0 ; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
+		for (int i = 0 ; i < idades.length; i++) {
+			System.out.println("Idades: " + idades[i]);
+		}
+		
 	}
 }
---
+++ bank-herdado/src/br/com/bytebank/banco/teste/TesteObject.java
@@ -0,0 +1,27 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.model.Cliente;
+import br.com.bytebank.banco.model.ContaCorrente;
+import br.com.bytebank.banco.model.ContaPoupanca;
+
+public class TesteObject {
+	public static void main(String[] args) {
+		
+		System.out.println("x");
+		System.out.println(3);
+		System.out.println(true);
+		
+		Object cc = new ContaCorrente(22, 2020);
+		Object cp = new ContaPoupanca(22, 32);
+		Object cliente = new Cliente("Mario Luiz", "027.789.789-85", "Desenvolvedor");
+		
+		System.out.println(cc.toString());
+		System.out.println(cp.toString());
+		
+		println(cliente);
+	}
+	
+	static void println(Object referencia) {
+	System.out.println(referencia.toString());
+	}
+}
