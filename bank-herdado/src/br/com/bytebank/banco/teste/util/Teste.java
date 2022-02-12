f738c1e5be2e670f6df10912621a163c3750760e
MarioLuiz <marioluiz.arruda@gmail.com>
2022-02-11 21:22:54 -0400

    Teste com ArrayList imprimindo inversamente

--- bank-herdado/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bank-herdado/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -10,7 +10,7 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		
-		ArrayList lista =  new ArrayList();
+		ArrayList<Conta> lista =  new ArrayList();
 		
 		Conta cc = new ContaCorrente(11,22);
 		Conta cc1 = new ContaCorrente(12,23);
@@ -32,12 +32,25 @@ public class Teste {
 		Conta cc3 = new ContaCorrente(55,55);
 		Conta cc4 = new ContaCorrente(66,66);
 		Conta cc5 = new ContaCorrente(77,77);
+		Conta cc6 = new ContaCorrente(88,88);
+		Conta cc7 = new ContaCorrente(99,99);
+		Conta cc8 = new ContaCorrente(1111,1111);
+		Conta cc9 = new ContaCorrente(2222,2222);
 		
-		lista.addAll(Arrays.asList(cc2,cc3,cc4,cc5));
+		System.out.println("Tamanho: " + lista.size());
+		
+		lista.addAll(Arrays.asList(cc2,cc3,cc4,cc5,cc6,cc7,cc8,cc9));
 		
 		for (Object object : lista) {
 			Conta c = (Conta) object;
 			System.out.println(c);
+		}
+		
+		System.out.println("Tamanho: " + lista.size());
+		
+		for (int i = lista.size() - 1; i >= 0; i--) {
+			Conta c = (Conta) lista.get(i);
+			System.out.println("Miau: " + c);
 		}
 		
 	}
