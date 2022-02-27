518b9822fdca729a3b1c22c83fb4e33883c116bb
MarioLuiz <marioluiz.arruda@gmail.com>
2022-02-26 20:36:38 -0400

    Comentando trechos

--- java-io/src/br/com/curso/alura/java/io/teste/TesteLeitura.java
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteLeitura.java
@@ -10,11 +10,16 @@ public class TesteLeitura {
 	public static void main(String[] args) throws IOException {
 		// Fluxo de entrada com arquivo
 		
+		//Encontrando arquivo
 		FileInputStream fis = new FileInputStream("lorem.txt");
+		
+		// convertando de binario para um reader
 		InputStreamReader isr = new InputStreamReader(fis);
+		
+		// utilizando um buffer para carregar a string
 		BufferedReader br =  new BufferedReader(isr);
 		
-		
+		// salvando primeira linha do doc
 		String linha = br.readLine();
 		
 		System.out.println(linha);
