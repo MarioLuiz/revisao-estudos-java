04c01e403ba340183ce0b1e813f9f9d71899b1a8
MarioLuiz <marioluiz.arruda@gmail.com>
2022-03-03 18:20:24 -0400

    Teste PrintStream e PrintWriter

---
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintWriter.java
@@ -0,0 +1,23 @@
+package br.com.curso.alura.java.io.teste;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class TesteEscritaPrintWriter {
+
+	public static void main(String[] args) throws IOException {
+		// Fluxo de entrada com arquivo
+		// Padrao de projeto decorator
+		// Criando o arquivo ou sobreencrendo, caso ja exista
+		PrintStream ps =  new PrintStream(new File("lorem4.txt"));
+
+		ps.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ");
+		ps.println();
+		ps.print("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam ");
+		
+		ps.close();
+
+	}
+
+}
