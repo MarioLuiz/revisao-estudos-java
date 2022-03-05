e503aae7521464511e8f8f5b9084dfe3256f054c
MarioLuiz <marioluiz.arruda@gmail.com>
2022-03-04 22:00:22 -0400

    Teste com Scanner

--- java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStream.java
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStream.java
@@ -2,22 +2,19 @@ package br.com.curso.alura.java.io.teste;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TesteEscritaPrintStream {
 
 	public static void main(String[] args) throws IOException {
-		// Fluxo de entrada com arquivo
-		// Padrao de projeto decorator
-		// Criando o arquivo ou sobreencrendo, caso ja exista
-		PrintWriter ps =  new PrintWriter(new File("lorem4.txt"));
-
-		ps.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ");
-		ps.println();
-		ps.print("tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam ");
 		
-		ps.close();
-
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		
+		while(scanner.hasNextLine()) {
+			String  linha = scanner.nextLine();
+			System.out.println(linha);
+		}
+		scanner.close();
 	}
 
 }
---
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,34 @@
+package br.com.curso.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws IOException {
+		// Fluxo de entrada com arquivo
+		// Padrao de projeto decorator
+		// Encontrando arquivo
+		FileInputStream fis = new FileInputStream("lorem.txt");
+
+		// convertando bits para Caracteres
+		InputStreamReader isr = new InputStreamReader(fis);
+
+		// Juntando varios caracteres dentro de uma linha
+		BufferedReader br = new BufferedReader(isr);
+
+		// salvando primeira linha do doc
+		String linha = br.readLine();
+		
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+
+		br.close();
+
+	}
+
+}
