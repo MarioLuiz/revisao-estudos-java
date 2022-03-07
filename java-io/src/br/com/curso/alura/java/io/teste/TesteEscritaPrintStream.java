0952be9cecc10bc3f24253ffa37d8f84ac4ba702
MarioLuiz <marioluiz.arruda@gmail.com>
2022-03-06 20:06:21 -0400

    Parseando o arquivo

--- java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStream.java
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStream.java
@@ -2,6 +2,8 @@ package br.com.curso.alura.java.io.teste;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteEscritaPrintStream {
@@ -13,7 +15,25 @@ public class TesteEscritaPrintStream {
 		while(scanner.hasNextLine()) {
 			String  linha = scanner.nextLine();
 			System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+            int valor2 = linhaScanner.nextInt();
+            int valor3 = linhaScanner.nextInt();
+            String valor4 = linhaScanner.next();
+            double valor5 = linhaScanner.nextDouble();
+			
+            System.out.println(valor1 +" "+ valor2 +" "+ valor3 +" "+ valor4 +" "+ valor5);
+            
+            linhaScanner.close();
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(Arrays.toString(valores));
 		}
+		
 		scanner.close();
 	}
 
