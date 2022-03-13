b577f380c41f6645c47ba56418c4060c08d3eca0
MarioLuiz <marioluiz.arruda@gmail.com>
2022-03-12 21:51:52 -0400

    Correção comentario

--- java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStreamCsv.java
+++ java-io/src/br/com/curso/alura/java/io/teste/TesteEscritaPrintStreamCsv.java
@@ -26,7 +26,7 @@ public class TesteEscritaPrintStreamCsv {
             String titular = linhaScanner.next();
             double saldo = linhaScanner.nextDouble();
 			
-			// Criando um String format para facilitar e padrinizar a saida dos dados
+			// Criando um String format para facilitar e padronizar a saida dos dados
             String valorFormatado = String.format(new Locale("pt","BR"),"%s - %04d - %d - %s - %.2f %n", tipoConta,agencia,numero,titular,saldo);
             System.out.println(valorFormatado);
             
