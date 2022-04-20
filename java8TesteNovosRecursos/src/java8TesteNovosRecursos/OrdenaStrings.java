b1499dc8a713ddcf3527991d6fd893cf378f2729
MarioLuiz <marioluiz.arruda@gmail.com>
2022-04-19 20:46:15 -0400

    Treinando com Lambdas e classes anonimas

--- java8TesteNovosRecursos/src/java8TesteNovosRecursos/OrdenaStrings.java
+++ java8TesteNovosRecursos/src/java8TesteNovosRecursos/OrdenaStrings.java
@@ -13,32 +13,46 @@ public class OrdenaStrings {
 		nomes.addAll(
 				Arrays.asList("Mario", "Luiz", "Arruda", "Revihery", "Claudio", "Iork", "Mia", "Carolina Beatriz"));
 
-		Comparator<String> comparadorTamanho = new ComparadorPorTamanho();
+		//Comparator<String> comparadorTamanho = new ComparadorPorTamanho();
 		// Collections.sort(nomes, comparadorTamanho);
-		nomes.sort(comparadorTamanho);
+		//nomes.sort(comparadorTamanho);
 		// System.out.println(nomes);
+		
+		// Sort com Lambda
+		nomes.sort((s1, s2) -> {
+			return s1.length() - s2.length();
+		});
+		
+		// Sort com Lambda utiliznado Integer.compare
+		nomes.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
 		nomes.forEach(nome -> System.out.println(nome));
 		System.out.println();
-		
-		//Utilizando consumer
-		Consumer<String> consumidor =  new ImprimeNaLinha();
+
+		// Utilizando consumer Classe anonima
+		Consumer<String> consumidor = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+
+		};
 		nomes.forEach(consumidor);
 	}
 }
 
-class ImprimeNaLinha implements Consumer<String> {
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
+//class ImprimeNaLinha implements Consumer<String> {
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//	}
+//}
 
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		return s1.length() - s2.length();
-	}
-
-}
+//class ComparadorPorTamanho implements Comparator<String> {
+//
+//	@Override
+//	public int compare(String s1, String s2) {
+//		return s1.length() - s2.length();
+//	}
+//
+//}
